7d796cb1152cedefdc729b6132327bbdc889d014
peter-zxh <zxh2409197195@163.com>
2020-10-28 09:27:46 +0800

    Site updated: 2020-10-28 09:27:33

--- js/main.js
+++ js/main.js
@@ -1,248 +1,144 @@
-/* eslint-disable no-undef */
-// console.js
-'use strict';
-console.log(String.raw`
-          _____                    _____                    _____                    _____          
-         /\    \                  /\    \                  /\    \                  /\    \         
-        /::\____\                /::\    \                /::\    \                /::\    \        
-       /:::/    /               /::::\    \               \:::\    \              /::::\    \       
-      /:::/    /               /::::::\    \               \:::\    \            /::::::\    \      
-     /:::/    /               /:::/\:::\    \               \:::\    \          /:::/\:::\    \     
-    /:::/____/               /:::/__\:::\    \               \:::\    \        /:::/__\:::\    \    
-   /::::\    \              /::::\   \:::\    \               \:::\    \      /::::\   \:::\    \   
-  /::::::\____\________    /::::::\   \:::\    \               \:::\    \    /::::::\   \:::\    \  
- /:::/\:::::::::::\    \  /:::/\:::\   \:::\    \               \:::\    \  /:::/\:::\   \:::\    \ 
-/:::/  |:::::::::::\____\/:::/  \:::\   \:::\____\_______________\:::\____\/:::/__\:::\   \:::\____\
-\::/   |::|~~~|~~~~~     \::/    \:::\  /:::/    /\::::::::::::::::::/    /\:::\   \:::\   \::/    /
- \/____|::|   |           \/____/ \:::\/:::/    /  \::::::::::::::::/____/  \:::\   \:::\   \/____/ 
-       |::|   |                    \::::::/    /    \:::\~~~~\~~~~~~         \:::\   \:::\    \     
-       |::|   |                     \::::/    /      \:::\    \               \:::\   \:::\____\    
-       |::|   |                     /:::/    /        \:::\    \               \:::\   \::/    /    
-       |::|   |                    /:::/    /          \:::\    \               \:::\   \/____/     
-       |::|   |                   /:::/    /            \:::\    \               \:::\    \         
-       \::|   |                  /:::/    /              \:::\____\               \:::\____\        
-        \:|   |                  \::/    /                \::/    /                \::/    /        
-         \|___|                   \/____/                  \/____/                  \/____/         
-see theme at https://github.com/theme-kaze/hexo-theme-kaze
-`);
-// darkmode.js
-// reverse button
-const scrollWidth = document.body.scrollWidth || document.documentElement.scrollWidth;
-let darkControlButton = null;
-let searchControlButton = null;
-if (scrollWidth <= 742) {
-  darkControlButton = document.querySelector('.darkwidget');
-  searchControlButton = document.querySelector('.searchwidget');
-} else {
-  darkControlButton = document.querySelector('.darknavbar');
-  searchControlButton = document.querySelector('.searchnavbar');
-}
+require([], function (){
 
-darkControlButton.addEventListener('click', () => {
-  setDarkmode(reverseDarkModeSetting());
-});
-// scroll-up.js
-const smoothScrollToTop = () => {
-  let yTopValve = (window.pageYOffset || document.body.scrollTop || document.documentElement.scrollTop);
-  if (yTopValve > 1) {
-    window.requestAnimationFrame(smoothScrollToTop);
-    scrollTo(0, Math.floor(yTopValve * 0.85));
-  } else {
-    scrollTo(0, 0);
-  }
-};
-setTimeout(() => {
-  document.getElementById('scrollbutton').onclick = smoothScrollToTop;
-}, 0);
-// popbutton.js
-const reversePopButton = () => {
-  const scrollButton = document.getElementById('scrollbutton');
-  const menuButton = document.getElementById('menubutton');
-  const reverseButton = document.getElementById('popbutton');
-  const scrollWidth = document.body.scrollWidth || document.documentElement.scrollWidth;
-  if (scrollButton.style.display === 'flex') {
-    scrollButton.style.bottom = '32px';
-    scrollButton.style.opacity = '0';
-    reverseButton.style.transform = 'none';
-    setTimeout(() => {
-      scrollButton.style.display = 'none';
-    }, 100);
-  } else {
-    scrollButton.style.display = 'flex';
-    reverseButton.style.transform = 'rotate(90deg)';
-    setTimeout(() => {
-      scrollButton.style.bottom = '85px';
-      scrollButton.style.opacity = '1';
-    }, 100);
-  }
-  const mobileToc = document.getElementById('mobiletoc');
-  if (scrollWidth <= 862 && mobileToc) {
-    if (menuButton.style.display === 'flex') {
-      menuButton.style.right = '32px';
-      menuButton.style.opacity = '0';
-      setTimeout(() => {
-        menuButton.style.display = 'none';
-      }, 100);
-    } else {
-      menuButton.style.display = 'flex';
-      setTimeout(() => {
-        menuButton.style.right = '85px';
-        menuButton.style.opacity = '1';
-      }, 100);
-    }
-  }
-  const darkButton = document.querySelector('.darkwidget');
-  const searchButton = document.querySelector('.searchwidget');
-  if (scrollWidth <= 742) {
-    if (darkButton.style.display === 'flex') {
-      darkButton.style.bottom = '32px';
-      darkButton.style.opacity = '0';
-      darkButton.style.transform = 'none';
-      setTimeout(() => {
-        darkButton.style.display = 'none';
-      }, 100);
-    } else {
-      darkButton.style.display = 'flex';
-      reverseButton.style.transform = 'rotate(90deg)';
-      setTimeout(() => {
-        darkButton.style.bottom = '138px';
-        darkButton.style.opacity = '1';
-      }, 100);
-    }
-
-    if (searchButton.style.display === 'flex') {
-      searchButton.style.bottom = '32px';
-      searchButton.style.opacity = '0';
-      searchButton.style.transform = 'none';
-      setTimeout(() => {
-        searchButton.style.display = 'none';
-      }, 100);
-    } else {
-      searchButton.style.display = 'flex';
-      searchButton.style.transform = 'rotate(90deg)';
-      setTimeout(() => {
-        searchButton.style.bottom = '191px';
-        searchButton.style.opacity = '1';
-      }, 100);
-    }
-  } 
-};
-setTimeout(() => {
-  document.getElementById('popbutton').onclick = reversePopButton;
-}, 0);
-// menuButton.js
-function menuClick(event) {
-  const target = event.target;
-  const mobileToc = document.getElementById('mobiletoc');
-  if (!mobileToc) {
-    return;
-  }
-  if (!mobileToc.contains(target)) {
-    mobileToc.style.display = 'none';
-    mask.remove();
-    document.removeEventListener('click', menuClick);
-  }
-}
-const clickMenuButton = () => {
-  const mobileToc = document.getElementById('mobiletoc');
-  if (!mobileToc) {
-    return;
-  }
-  mobileToc.style.display = 'block';
-  const mask = document.createElement('div');
-  mask.id = 'mask';
-  document.body.append(mask);
-  setTimeout(() => {
-    document.addEventListener('click', menuClick);
-  }, 0);
-};
-setTimeout(() => {
-  document.getElementById('menubutton').onclick = clickMenuButton;
-}, 0);
-// search.js
-// search button
-
-function searchClick(event) {
-  const searchContent = document.querySelector('#local-search');
-  if(!searchContent.contains(event.target)) {
-    const searchInput = document.querySelector('#search-input');
-    const content = document.querySelector('#search-content');
-    searchInput.value = '';
-    searchContent.style.display = 'none';
-    content.innerHTML = '';
-    mask.remove();
-    document.removeEventListener('click', searchClick);
-  }
-}
-
-setTimeout(() => {
-  searchControlButton.addEventListener('click', () => {
-    const mask = document.createElement('div');
-    mask.id = 'mask';
-    document.body.append(mask);
-    const searchMain = document.querySelector('#local-search');
-    searchMain.style.display = 'block';
-    setTimeout(() => {
-      document.addEventListener('click', searchClick);
-    }, 0);
-  });
-});
-
-const localSearch = function (path) {
-  fetch(path)
-    .then(res => res.json())
-    .then(res => {
-      let input = document.getElementById('search-input');
-      let resultContent = document.getElementById('search-content');
-      
-      input.addEventListener('input', function () {
-        let str = '<ul class="search-result-list">';
-        let keyword = this.value.trim().toLowerCase();
-        resultContent.innerHTML = '';
-        if (this.value.trim().length <= 0) {
-          return;
-        }
-        res.forEach(function (data) {
-          let isMatch = true;
-          if (!data.title || data.title.trim() === '') {
-            data.title = 'Untitled';
-          }
-          const dataTitle = data.title.trim().toLowerCase();
-          const dataContent = data.content.trim().replace(/<[^>]+>/g, '').toLowerCase();
-          let firstOccur = -1;
-          if (dataContent !== '') {
-            const indexTitle = dataTitle.indexOf(keyword);
-            const indexContent = dataContent.indexOf(keyword);
-            firstOccur = indexContent;
-            if (indexTitle < 0 && indexContent < 0) {
-              isMatch = false;
-            } else if (indexContent < 0) {
-              firstOccur = 0;
-            }
-          } else {
-            isMatch = false;
-          }
-          if (isMatch) {
-            str += '<li><a href=\'' + data.url + '\' class=\'search-result-title\'>' + dataTitle + '</a>';
-            const content = data.content;
-            if (firstOccur >= 0) {
-              const start = Math.max(0, firstOccur - 12);
-              const end = Math.min(content.length, firstOccur + 12);
-              let matchContent = content.substr(start, end);
-              matchContent = matchContent.replace(new RegExp(keyword, 'gi'), '<em class="search-keyword">' + keyword + '</em>');
-              str += '<p class="search-result">' + matchContent + '...</p>';
-            }
-            str += '</li>';
-          }
+    var isMobileInit = false;
+    var loadMobile = function(){
+        require([yiliaConfig.rootUrl + 'js/mobile.js'], function(mobile){
+            mobile.init();
+            isMobileInit = true;
         });
-        str += '</ul>';
-        if (str.indexOf('<li>') === -1) {
-          return resultContent.innerHTML = '<ul><span class="local-search-empty">没有搜索到结果<span></ul>';
+    }
+    var isPCInit = false;
+    var loadPC = function(){
+        require([yiliaConfig.rootUrl + 'js/pc.js'], function(pc){
+            pc.init();
+            isPCInit = true;
+        });
+    }
+
+    var browser={
+        versions:function(){
+        var u = window.navigator.userAgent;
+        return {
+            trident: u.indexOf('Trident') > -1, //IE内核
+            presto: u.indexOf('Presto') > -1, //opera内核
+            webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
+            gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
+            mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+            ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+            android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或者uc浏览器
+            iPhone: u.indexOf('iPhone') > -1 || u.indexOf('Mac') > -1, //是否为iPhone或者安卓QQ浏览器
+            iPad: u.indexOf('iPad') > -1, //是否为iPad
+            webApp: u.indexOf('Safari') == -1 ,//是否为web应用程序，没有头部与底部
+            weixin: u.indexOf('MicroMessenger') == -1 //是否为微信浏览器
+            };
+        }()
+    }
+
+    $(window).bind("resize", function(){
+        if(isMobileInit && isPCInit){
+            $(window).unbind("resize");
+            return;
         }
-        resultContent.innerHTML = str;
-      });
+        var w = $(window).width();
+        if(w >= 700){
+            loadPC();
+        }else{
+            loadMobile();
+        }
     });
-};
-setTimeout(() => {
-  localSearch('/search.json');
-}, 0);
+
+    if(browser.versions.mobile === true || $(window).width() < 700){
+        loadMobile();
+    }else{
+        loadPC();
+    }
+
+    //是否使用fancybox
+    if(yiliaConfig.fancybox === true){
+        require([yiliaConfig.rootUrl + 'fancybox/jquery.fancybox.js'], function(pc){
+            var isFancy = $(".isFancy");
+            if(isFancy.length != 0){
+                var imgArr = $(".article-inner img");
+                for(var i=0,len=imgArr.length;i<len;i++){
+                    var src = imgArr.eq(i).attr("src");
+                    var title = imgArr.eq(i).attr("alt");
+                    imgArr.eq(i).replaceWith("<a href='"+src+"' title='"+title+"' rel='fancy-group' class='fancy-ctn fancybox'><img src='"+src+"' title='"+title+"'></a>");
+                }
+                $(".article-inner .fancy-ctn").fancybox();
+            }
+        });
+
+    }
+    //是否开启动画
+    if(yiliaConfig.animate === true){
+
+        require([yiliaConfig.rootUrl + 'js/jquery.lazyload.js'], function(){
+            //avatar
+            $(".js-avatar").attr("src", $(".js-avatar").attr("lazy-src"));
+            $(".js-avatar")[0].onload = function(){
+                $(".js-avatar").addClass("show");
+            }
+        });
+
+      if(yiliaConfig.isHome === true) {
+        // 滚动条监听使用scrollreveal.js
+        // https://github.com/jlmakes/scrollreveal.js
+        // 使用cdn[//cdn.bootcss.com/scrollReveal.js/3.0.5/scrollreveal.js]
+        require([
+          '//cdn.bootcss.com/scrollReveal.js/3.0.5/scrollreveal.js'
+        ], function (ScrollReveal) {
+          // 更多animation:
+          // http://daneden.github.io/animate.css/
+          var animationNames = [
+            "pulse", "fadeIn","fadeInRight", "flipInX", "lightSpeedIn","rotateInUpLeft", "slideInUp","zoomIn",
+            ],
+            len = animationNames.length,
+            randomAnimationName = animationNames[Math.ceil(Math.random() * len) - 1];
+
+          // ie9 不支持css3 keyframe动画, safari不支持requestAnimationFrame, 不使用随机动画，切回原来的动画
+          if (!window.requestAnimationFrame) {
+              $('.body-wrap > article').css({opacity: 1});
+
+              if (navigator.userAgent.match(/Safari/i)) {
+                  function showArticle(){
+                      $(".article").each(function(){
+                          if( $(this).offset().top <= $(window).scrollTop()+$(window).height() && !($(this).hasClass('show')) ) {
+                              $(this).removeClass("hidden").addClass("show");
+                              $(this).addClass("is-hiddened");
+                          }else{
+                              if(!$(this).hasClass("is-hiddened")){
+                                  $(this).addClass("hidden");
+                              }
+                          }
+                      });
+                  }
+                  $(window).on('scroll', function(){
+                      showArticle();
+                  });
+                  showArticle();
+              }
+              return;
+          }
+          // document.body有些浏览器不支持监听scroll，所以使用默认的document.documentElement
+          ScrollReveal({
+            duration: 0,
+            afterReveal: function (domEl) {
+              // safari不支持requestAnimationFrame不支持document.documentElement的onscroll所以这里不会执行
+              // 初始状态设为opacity: 0, 动画效果更平滑一些(由于脚本加载是异步，页面元素渲染后在执行动画，感觉像是延时)
+              $(domEl).addClass('animated ' + randomAnimationName).css({opacity: 1});
+            }
+          }).reveal('.body-wrap > article');
+
+        });
+      } else {
+        $('.body-wrap > article').css({opacity: 1});
+      }
+
+    }
+
+    //是否新窗口打开链接
+    if(yiliaConfig.open_in_new == true){
+        $(".article a[href]").attr("target", "_blank")
+    }
+    $(".archive-article-title").attr("target", "_blank");
+});
